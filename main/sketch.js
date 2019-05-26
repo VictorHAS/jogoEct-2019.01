9522caa998d498f8ee38aead3b57ffeeda8c5500
VictorHAS <victorhermesalves@gmail.com>
2019-05-25 23:31:45 -0300

    atirar em direção do mouse, colisão bala -> inimigo

--- main/sketch.js
+++ main/sketch.js
@@ -2,18 +2,22 @@
 var canJump = false;
 //-------------------
 //-----Variaveis-----
+var dx, dy, targetX, targetY;
 var balas;
 var font;
 var xa, xy;
 var bg;
-var tela = 3;
+var tela = 1;
 var angle1 = 0.0,
   segLength = 90;
 var contFrames = 0;
 //-------------------
 //-----Cenario-----
-var wall1;
 var plataformB;
+//-------------------
+//---Inimigo Info---
+var inimigos;
+var qnt = 3;
 //-------------------
 //---Jogador Info---
 var arma;
@@ -21,9 +25,10 @@ var direcao;
 var posXI = 390;
 var posYI = 300;
 var lifes = 3;
+var pontos = 0;
 var jogador, arma;
 var GRAVITY = 0.5;
-var JUMP = 15;
+var JUMP = 20;
 var armaImg, plataformImg;
 //-------------------
 //-----Animações-----
@@ -36,43 +41,37 @@ var bala_anim;
 //-------------------
 function preload() {
   arma_img = loadImage("../assets/arma.png");
-  plataformImg = loadImage("../assets/plataforma/PlataformaBaixo.png");
-  idle_anim = loadAnimation(
-    "../assets/idle/Armature_IDLE_00.png",
-    "../assets/idle/Armature_IDLE_39.png"
+  plataformImg = loadImage("../assets/PlataformaBaixo.png");
+  idle_anim = loadImage("../assets/player_idle.png");
+  gameover_anim = loadAnimation(
+    "../assets/gameover/gameover01.png",
+    "../assets/gameover/gameover99.png"
   );
-  gameover_anim = loadAnimation("../assets/gameover/gameover01.png", "../assets/gameover/gameover99.png")
-  jump_anim = loadAnimation(
-    "../assets/jump/Armature_JUMP_16.png",
-    "../assets/jump/Armature_JUMP_37.png"
-  );
+  jump_anim = loadImage("../assets/player_jump.png");
   run_anim = loadAnimation(
-    "../assets/walk/Armature_RUN_00.png",
-    "../assets/walk/Armature_RUN_15.png"
+    "../assets/player_walk1.png",
+    "../assets/player_walk2.png"
   );
-  bala_anim = loadImage("../assets/Bullet.png")
+  bala_anim = loadImage("../assets/Bullet.png");
   bg = loadImage("../assets/background0.png");
   font = loadFont("../assets/True2D.ttf");
 }
 function setup() {
-  createCanvas(720, 480);
+  angleMode(RADIANS);
+  createCanvas(1280, 500);
   frameRate(60);
-  wall1 = createSprite(width / 2, 200, 500, 40);
-  wall1.setCollider("rectangle", 0, 0, 500, 40);
-  wall1.shapeColor = color(150, 255);
-
   jogador = createSprite(posXI, posYI, 50, 100);
-  jogador.setCollider("rectangle", 0, 0, 350, 400);
+  jogador.setDefaultCollider();
   //jogador.debug = true;
   jogador.shapeColor = color(25, 84);
   jogador.friction = 0.05;
   jogador.addAnimation("normal", idle_anim);
   jogador.addAnimation("jump", jump_anim);
   jogador.addAnimation("walk", run_anim);
-  jogador.scale = 0.2;
-
+  jogador.scale = 1;
+  qnt = 3;
   balas = new Group();
-
+  inimigos = new Group();
   plataformB = createSprite(width / 2, 500, 200, 200);
   plataformB.setDefaultCollider();
   plataformB.addImage(plataformImg);
@@ -85,11 +84,7 @@ function draw() {
     textSize(40);
     textAlign(CENTER);
     fill(0);
-    text(
-      "Pressione Enter para Começar",
-      (width / 2) + 1,
-      (height / 2) + 1
-    );
+    text("Pressione Enter para Começar", width / 2 + 1, height / 2 + 1);
     fill(100);
     text("Pressione Enter para Começar", width / 2, height / 2);
     if (keyIsDown(13)) tela = 2;
@@ -101,16 +96,11 @@ function draw() {
     if (direcao < 0) {
       direcao *= -1;
     }
-    //----------------------DEBUG CODE-----------------------------//
-    //jogador.velocity.x = (mouseX - jogador.position.x) / 10;
-    //jogador.velocity.y = (mouseY - jogador.position.y) / 10;
-    //jogador.position.x = mouseX;
-    //jogador.position.y = mouseY;
     textFont(font);
     textSize(40);
     fill(100);
-    jogador.collide(wall1);
     text("Vidas: " + lifes, 30, 30);
+    text("Pontos: " + pontos, 30, 60);
     jogador.velocity.y += GRAVITY;
     if (jogador.position.y > height) {
       jogador.position.x = posXI;
@@ -124,15 +114,15 @@ function draw() {
       }
       jogador.position.x += 5;
       direcao = jogador.getDirection() - 90;
-      console.log(direcao)
+      console.log(direcao);
       jogador.mirrorX(1);
       if (jogador.position.x > width) jogador.position.x = 0;
     } else if (keyIsDown(LEFT_ARROW)) {
       if (canJump) {
         jogador.changeAnimation("walk");
       }
-      direcao = jogador.getDirection() + 90
-      console.log(direcao)
+      direcao = jogador.getDirection() + 90;
+      console.log(direcao);
       jogador.position.x -= 5;
       jogador.mirrorX(-1);
       if (jogador.position.x < 0) jogador.position.x = width;
@@ -141,52 +131,47 @@ function draw() {
         jogador.changeAnimation("normal");
       }
     }
-    dx = mouseX - jogador.position.x;
-    dy = mouseY - jogador.position.y;
-    angle1 = atan2(dy, dx);
     if (keyWentDown(CONTROL)) {
-      var bala = createSprite(jogador.position.x, jogador.position.y);
-      bala.setSpeed(10, direcao);
-      bala.addImage(bala_anim)
-      bala.scale = 0.1
-      bala.life = 100;
-      balas.add(bala);
+      atirar();
     }
-    if (jogador.collide(plataformB)) {
-      jump_anim.goToFrame(0);
+    balas.overlap(inimigos, collect);
+    while (jogador.collide(plataformB)) {
       canJump = true;
       jogador.velocity.y = 0;
     }
     if (keyIsDown(UP_ARROW)) {
       if (canJump) {
         if (keyIsDown(RIGHT_ARROW)) {
-          direcao = jogador.getDirection() - 90
+          direcao = jogador.getDirection() - 90;
         }
         if (keyIsDown(LEFT_ARROW)) {
-          direcao = jogador.getDirection() + 90
+          direcao = jogador.getDirection() + 90;
         }
         jogador.changeAnimation("jump");
         jogador.velocity.y = -JUMP;
         canJump = false;
       }
     }
+    if (pontos <= 150) {
+      qnt = 5;
+      for (var i = 0; i < inimigos.length; i++) {
+        var s = inimigos[i];
+        s.setSpeed(2, 180);
+      }
+    } else if (pontos >= 250) {
+      qnt = 7;
+      for (var i = 0; i < inimigos.length; i++) {
+        var s = inimigos[i];
+        s.setSpeed(4, 180);
+      }
+    }
+    if (inimigos.length <= qnt) {
+      desenharInimigos();
+    }
     drawSprites();
-    //segment(jogador.position.x + 20, jogador.position.y + 20, angle1);
   }
   if (tela == 3) {
-    animation(gameover_anim, width / 2, height / 2)
-    /*textFont(font);
-    textSize(80);
-    textAlign(CENTER);
-    fill(0)
-    text("GAME OVER\n aperte enter para recomeçar", (width / 2) + 1,
-      (height / 2) + 1)
-    fill(204, 0, 0);
-    text(
-      "GAME OVER\n aperte enter para recomeçar",
-      width / 2,
-      height / 2
-    );*/
+    animation(gameover_anim, width / 2, height / 2);
     lifes = 3;
     jogador.velocity.x = 0;
     jogador.position.x = posXI;
@@ -200,7 +185,33 @@ function segment(x, y, a) {
   push();
   translate(x, y);
   rotate(a);
-  image(armaImg, 0, 0, 0, 0);
-  //line(0, 0, segLength, 0);
+  line(0, 0, segLength, 0);
   pop();
+}
+function atirar() {
+  let a = atan2(
+    (jogador.position.y - mouseY) * -1,
+    (jogador.position.x - mouseX) * -1
+  );
+  var bala = createSprite(jogador.position.x, jogador.position.y);
+  console.log(cos(a) + " " + sin(a));
+  bala.velocity.x = cos(a);
+  bala.velocity.y = sin(a);
+  bala.setSpeed(5);
+  //bala.setSpeed(10, a);
+  bala.addImage(bala_anim);
+  bala.scale = 0.1;
+  //bala.life = 200;
+  balas.add(bala);
+}
+function desenharInimigos() {
+  var inmg = createSprite(1400, random(200, 350), 30, 30);
+  inmg.setDefaultCollider();
+  inmg.setSpeed(1, 180);
+  inimigos.add(inmg);
+}
+function collect(collector, collected) {
+  pontos += random(2, 20);
+  collector.remove();
+  collected.remove();
 }
